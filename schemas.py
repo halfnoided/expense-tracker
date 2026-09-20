bc026323057563ec3e6f46e1c5ee131f4f709c0b
halfnoided <halfnoided@gmail.com>
2026-09-20 11:38:37 +0300

    initial commit: setup for fastapi expense tracker, Transaction schema added.

---
+++ schemas.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+from datetime import datetime
+from enum import Enum
+
+class TransactionType(str, Enum):
+    expense = "expense"
+    income = "income"
+
+class Transaction(BaseModel):
+    transaction_type: TransactionType
+    value: float
+    description: str
+    date: datetime
+    category: str
